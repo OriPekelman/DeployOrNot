a665584d78d71378859d216c6592f0a6f0fbfb17
Ori Pekelman <ori@platform.sh>
2016-10-24 13:38:07 +0200

    try to see if we can get a nice realtime output

--- app/platform.php
+++ app/platform.php
@@ -15,6 +15,6 @@ while (ob_get_level() > 0) {
     ob_end_clean();
     // If the current level has not changed, abort
     if (ob_get_level() == $level) break;
-
-$command = "for i in {1..10}; do platform environment:info -p qa4xawahw66fk -e with_cli;sleep 1; done;"
+  }
+$command = "for i in {1..10}; do platform environment:info -p qa4xawahw66fk -e with_cli;sleep 1; done;";
 system($command);
