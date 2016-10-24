f265d25b629d698d6e51fdf57b8e822312c6dd82
Ori Pekelman <ori@platform.sh>
2016-10-24 11:18:20 +0200

    correted PHP notices

--- api.php
+++ api.php
@@ -1,11 +1,11 @@
 <?php
-  require_once "vendor/autoload.php";
-  require_once 'HTTP/Request2.php';
+require_once "vendor/autoload.php";
+require_once 'HTTP/Request2.php';
 
-  header('Content-Type: application/json');
+header('Content-Type: application/json');
 
-  // Emotion API Subscription key
-  $key = $_ENV['Emotion_API_Key'];
+// Emotion API Subscription key
+if($key = getenv('EMOTION_API_KEY')){ 
 
   $happy = false;
   $result = "";
@@ -13,23 +13,20 @@
 
   //Getting form POST values
   $filename = $_FILES['file']['tmp_name'];
-  $param1 = $_POST['param1'];
-  $param2 = $_POST['param2'];
-
   //Calling the Cognitive Services API
   $request = new Http_Request2('https://api.projectoxford.ai/emotion/v1.0/recognize');
   $url = $request->getUrl();
 
   $headers = array(
-      // Request headers
-      'Content-Type' => 'application/octet-stream',
-      'Ocp-Apim-Subscription-Key' => $key,
+    // Request headers
+    'Content-Type' => 'application/octet-stream',
+    'Ocp-Apim-Subscription-Key' => $key,
   );
 
   $request->setHeader($headers);
 
   $parameters = array(
-      // Request parameters
+    // Request parameters
   );
 
   $url->setQueryVariables($parameters);
@@ -47,16 +44,16 @@
 
   try
   {
-      $response = $request->send();
-      $arr = json_decode($response->getBody(), true);
-      if(round($arr[0]['scores']['happiness']) == 1)
-      {
-        $happy = true;
-      }
+    $response = $request->send();
+    $arr = json_decode($response->getBody(), true);
+    if(round($arr[0]['scores']['happiness']) == 1)
+    {
+      $happy = true;
+    }
   }
   catch (HttpException $ex)
   {
-      $error = $ex;
+    $error = $ex;
   }
   
   //Calling the platform.sh API
@@ -64,13 +61,13 @@
   {
     //TODO
     $result = 'Déploiement confirmé !';
-  }
-  else
-  {
+  } else {
     //TODO
     $result = ('Déploiement annulé pour cause de mauvaise humeur !');
   }
-
-  // //Returning the result for display
-   echo ('{"result":"' . $result . '","error":"' . $error . '"}');
-?>
+} else {
+  $error = ('Clé API non fournie.');
+  $result = ('Humeur inconnue, déploiement réfusé !');
+}
+// //Returning the result for display
+echo ('{"result":"' . $result . '","error":"' . $error . '"}');
