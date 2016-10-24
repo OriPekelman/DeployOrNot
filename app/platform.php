827dc138a1d6ca7d3e1863fa5c9a04a78070137d
Ori Pekelman <ori@platform.sh>
2016-10-24 13:33:28 +0200

    try to see if we can get a nice realtime output

--- app/platform.php
+++ app/platform.php
@@ -1,2 +1,19 @@
 <?php
-echo `platform environment:info -p qa4xawahw66fk -e with_cli`;
+
+// Turn off output buffering
+ini_set('output_buffering', 'off');
+// Turn off PHP output compression
+ini_set('zlib.output_compression', false);
+// Implicitly flush the buffer(s)
+ini_set('implicit_flush', true);
+ob_implicit_flush(true);
+// Clear, and turn off output buffering
+while (ob_get_level() > 0) {
+    // Get the curent level
+    $level = ob_get_level();
+    // End the buffering
+    ob_end_clean();
+    // If the current level has not changed, abort
+    if (ob_get_level() == $level) break;
+
+echo `for i in {1..10}; do platform environment:info -p qa4xawahw66fk -e with_cli;sleep 1; done;`
