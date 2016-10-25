4a49e11808e2e352049a1531171e8aaa0d82c0a2
Ori Pekelman <ori@platform.sh>
2016-10-25 13:58:41 +0200

    pass both project and environment in url

---
+++ app/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html xmlns:fb="http://ogp.me/ns/fb" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DeployOrNot?</title>
+    <link href="/styles/css.css" rel="stylesheet">
+    <link href="/styles/webcam.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container" style="text-align: center; max-width: 800px;">
+        <div class="row center-block" id="header">
+            <div id="headertext">
+                <div class="row">
+                    <div>
+                        <h1 id="picmeh1id">DeployOrNot?</h1>
+                    </div>
+                    <div class="subheader">
+                        Forum PHP <span id="demoSpan">2016</span>
+                    </div>
+                    <div class="subheader">
+                        <img id="headerimage" src="/img/icon.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p id="error" class="alert--error"></p>
+        <button class="button btn btn-default action" type="button" id="start" style="display:none">Ready ?</button>
+        <div class="demo-area" id="democontent" style="display:none">
+            <h3 class="subtitle view-text view-text--video">
+                <span>Webcam view</span> <button type="button" style="background-image:url('/img/camera.png');background-size:contain;min-height:30px;min-width:30px; border:none" id="switch"></button>
+                <span id="videoViewText" class="view-text__instructions"></span>
+            </h3>
+            <div class="view--video">
+                <video id="videoTag" src="" autoplay muted class="view--video__video"></video>
+            </div>
+            <h3 class="subtitle view-text view-text--snapshot">
+                <span>Snapshot view</span>
+                <span id="photoViewText" class="view-text__instructions"></span>
+            </h3>
+            <div class="view--snapshot">
+                <canvas id="canvasTag" class="view--snapshot__canvas"></canvas>
+                <a id="saveImg" class="hide" href="#"></a>
+            </div>
+        </div>
+        <p id="tooltip" class="alert--error hide"></p>
+
+        <div style="display:inline">
+            <img id="photo1" style="max-height:225px;" />
+            <img id="photo2" style="max-height:225px;" />
+        </div>
+        <div id="result" style="font-size:x-large;color:white;"></div>
+        <div id="loading" style="display:none"><img src="/img/loading.gif" style="max-height:40px" /></div>
+        <br />
+        <button class="btn btn-default action" id="restartButton" style="display:none; border-radius: 0px; background-color: rgb(174, 117, 11)" onclick="window.location.reload()">
+            Restart
+        </button>
+        <div id="results" hidden="">
+            <h5 class="bodyfont" id="analyzingLabel" style="text-align: center; font-size: medium; visibility: hidden;"></h5>
+            <div class="center-block" id="thumbContainer" style="padding-left: 0px; position: relative;">
+                <img class="img-responsive center-block" id="thumbnail" onerror=" this.onerror=null;this.src ='/img/placeholder.png' ; "
+                     src="/img/placeholder.png">
+                <div id="faces">
+                    <div></div>
+                </div>
+            </div>
+            <div id="jsonEventDiv" style="margin-top: 30px;" hidden="">
+                <pre id="jsonEvent" style="text-align: left;"></pre>
+            </div>
+        </div>
+        <div id="console" style="width: 100%;height: 100%;">
+        </div>
+        <div class="help-block center-block bodyfont" style="margin-top:20px">
+            <span style="text-align: center; display: inline-block">
+                Powered by <a class="bodyfont link" id="dontWorryId" href="https://www.microsoft.com/cognitive-services" target="_blank">Microsoft Cognitive Services</a> & <a class="bodyfont link" id="dontWorryId" href="https://platform.sh/" target="_blank">platform.sh</a>
+            </span>
+        </div>
+        <div id="footerId" style="padding-top:20px">
+            <span style="width: 96px; font-size: 14px;">
+                <a class="bodyfont link" style="font-size:14px" href="https://twitter.com/Benjiiim" target="_blank">Benjamin Talmard</a> & <a style="font-size:14px" class="bodyfont link" href="https://twitter.com/OriPekelman" target="_blank">Ori Pekelman</a></span>
+            </span>
+        </div>
+        <input name="source" id="source" type="hidden">
+
+    </div>
+    <script src="/javascript/canvas-to-blob.min.js"></script>
+    <script src="/javascript/jquery-1.12.4.js"></script>
+    <script>
+    var project= "<?php echo preg_replace("/[^A-Za-z0-9 ]/", '', $_GET["project"]);?>";
+    var environment= "<?php echo preg_replace("/[^A-Za-z0-9 ]/", '', $_GET["environment"]);?>";
+    </script>
+    <script src="/javascript/webcam.js"></script>
+</body>
+</html>
